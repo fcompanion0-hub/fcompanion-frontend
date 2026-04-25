e09c5359a759643428f411df916883053fd49722
jaeger <jaegerdesu10@gmail.com>
2026-04-25 18:30:06 +0100

    splash screen font issue

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_STATIC = "fcompanion-static-v7";
-const CACHE_CHAT   = "fcompanion-chat-v7";
+const CACHE_STATIC = "fcompanion-static-v8";
+const CACHE_CHAT   = "fcompanion-chat-v8";
 
 // Static assets to pre-cache on install
 const STATIC_ASSETS = [
